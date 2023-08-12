9245ff89d783e35cad1bc53a7bc342b82a658bff
tzptzptzptzptzp <tzp2539@gmail.com>
2023-08-12 22:58:35 +0900

    🔧 gulp設定ファイルを作成

---
+++ gulp.config.js
@@ -0,0 +1,16 @@
+const dist = "dist";
+const src = "src";
+
+module.exports = {
+  path: {
+    dist: dist,
+    src: {
+      top: `${src}/index.ejs`,
+    },
+  },
+  settingf: {
+    server: {
+      port: 2525,
+    },
+  },
+};
