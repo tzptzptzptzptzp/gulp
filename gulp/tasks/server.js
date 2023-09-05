7ba4d2431e147d20e7c30d67a0dea47fde444ba4
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-06 00:09:46 +0900

    ⚡️ トップページが下層になる場合にスタートページを対応させる記述を追加

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -1,11 +1,15 @@
 const config = require('../config')
 const browserSync = require('browser-sync')
+const top = require('../../data/pages/top')
+
+const path = top[0].directory !== null ? top[0].directory : ''
 
 function server(done) {
   browserSync.init({
     server: `./${config.path.dist}`,
     reloadOnRestart: true,
     port: config.setting.server.port,
+    startPath: path,
   })
   done()
 }
